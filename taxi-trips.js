68b9fa506a250b300913cbfc625657077ed6c9b9
josephmabusela <josephmabusela5@gmail.com>
2021-11-22 13:29:24 +0200

    initial commit

--- taxi-trips.js
+++ taxi-trips.js
@@ -1,3 +1,49 @@
-module.exports = function() {
+module.exports = function TaxiTrips () {
 	
+    async function totalTripCount() {
+        
+    }
+
+    async function findAllRegions () {
+
+    }
+
+    async function findTaxisForRegion () {
+
+    }
+    async function findTripsByRegNumber () {
+
+    }
+
+    async function findTripsByRegion () {
+
+    }
+
+    async function findIncomeByRegNumber () {
+
+    }
+
+    async function findTotalIncomePerTaxi () {
+
+    }
+
+    async function findTotalIncome () {
+
+    }
+
+    async function findTotalIncomeByRegion () {
+
+    }
+
+    return {
+        totalTripCount,
+        findAllRegions,
+        findTaxisForRegion,
+        findTripsByRegNumber,
+        findTripsByRegion,
+        findIncomeByRegNumber,
+        findTotalIncomePerTaxi,
+        findTotalIncome,
+        findTotalIncomeByRegion
+    }
 }
